971f4dc2b13b206b76310cc288ad872711336175
CristianBanan <cristian.ursache96@yahoo.com>
2017-12-07 13:22:57 +0200

    change

---
+++ Animals/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    class Animal
+    {
+        // Code review: breaks encapsulation
+        public string name { get; set; }
+
+        public string sound { get; set; }
+
+        public Animal(string Name, string Sound)
+        {
+            name = Name;
+            sound = Sound;
+        }
+
+        public override string ToString()
+        {
+            return name + " makes " + sound; 
+        }
+
+    }
+}
