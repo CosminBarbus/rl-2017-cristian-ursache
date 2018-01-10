2ed493498ac9f207d56f21136c3f973802f410dd
CristianBanan <cristian.ursache96@yahoo.com>
2018-01-10 22:54:26 +0200

    session 5

--- Session5_VendingMachine/VendingMachine/VendingMachine/ProductCollection.cs
+++ Session5_VendingMachine/VendingMachine/VendingMachine/ProductCollection.cs
@@ -8,32 +8,31 @@ namespace VendingMachine
 {
     class ProductCollection
     {
-       
-       int[,] Cells = new int[3, 3]; // row 0 - for soda , 1 - snack, 2 - sandwich; coll 0 - size 1, coll 1 - size 2, coll 2 - size 3
 
+        /*
         //Categories
         static ProductCategory Soda = new ProductCategory("Soda");
         static ProductCategory Snack = new ProductCategory("Snack");
         static ProductCategory Sandwich = new ProductCategory("Sandwich");
 
         //Products
-        /* Soda */
+        // Soda 
         static Product CocaCola = new Product("Coca-Cola 250", 0.5, 1, Soda);
         static Product Sprite = new Product("Sprite 330", 0.75, 2, Soda);
         static Product Fanta = new Product("Fanta 500", 1, 3, Soda);
 
 
-        /* Snack */
+        // Snack 
         static Product Snickers = new Product("Snickers", 0.75, 1, Snack);
         static Product Cheetos = new Product("Cheetos", 1, 2, Snack);
         static Product Lays = new Product("Lays", 1.5, 3, Snack);
 
-        /* Sandwich */
+        // Sandwich
         static Product BaconSandwich = new Product("Bacon Sandwich", 1, 1, Sandwich);
         static Product Crisp = new Product("Crisp",1.5, 2, Sandwich);
         static Product Kebab = new Product("Kebab", 2, 3, Sandwich);
 
-
+        
         //ContainableItem
         ContainableItem sprite = new ContainableItem(Sprite);
         ContainableItem cocacola = new ContainableItem(CocaCola);
@@ -43,50 +42,64 @@ namespace VendingMachine
         ContainableItem lays = new ContainableItem(Lays);
         ContainableItem baconSandwich = new ContainableItem(BaconSandwich);
         ContainableItem crisp = new ContainableItem(Crisp);
-        ContainableItem kebab = new ContainableItem(Kebab);
+        ContainableItem kebab = new ContainableItem(Kebab); 
+        */
+        //Dispensers
+        /*
+        Dispenser cocaColaBand = new Dispenser(1);
+        Dispenser spriteBand = new Dispenser(2);
+        Dispenser fantaBand = new Dispenser(3);
 
+        Dispenser snickersBand = new Dispenser(1);
+        Dispenser cheetosBand = new Dispenser(2);
+        Dispenser laysBand = new Dispenser(3);
 
-        public bool Add(ContainableItem Item)
+        Dispenser baconSandwichBand = new Dispenser(1);
+        Dispenser CrispBand = new Dispenser(2);
+        Dispenser KebabBand = new Dispenser(3);
+        */
+
+        public bool Add(Dispenser band)
         {
-
-            if (Cells[Item.row, Item.col] < 10) //max space is 10
-            {
-                Cells[Item.row, Item.col]++;
+            if (band.AddItem())
                 return true;
-            }
-
             else
-            {
-                Console.WriteLine("Cannot add item!, row [%], coll [%] is full", Item.col, Item.row);
                 return false;
-            }
             
         }
        
-       public bool Remove(ContainableItem Item)
+       public bool Remove(Dispenser band)
        {
-           if (Cells[Item.row, Item.col] > 0)
-           {
-               Cells[Item.row, Item.col]--;
-               return true;
-           }
+           if (band.Dispose()) 
+               return true; 
            else
-               Console.WriteLine("Out of stock");
                return false;
         }
 
-       public int Count()
+        public int Count(List<Dispenser> dispensers)
         {
             int count = 0;
-            for (int i = 0; i < 3; i++)
-                for (int j = 0; j < 3; j++)
-                {
-                    count += Cells[i, j];
-                }
+            foreach (var disp in dispensers)
+            {
+                count += disp.Count();
+            }
             return count;
         }
+        public void Payment(float dollars)
+        {
+            //if()
+        }
 
-        public void GetItem()
+        public float GetItem(Dispenser band, float money)
+        {
+            if (band.Product.price <= money)
+            {
+                money -= (float)band.Product.price;
+                Remove(band);
+            }
+            return money;
+        }
+        /*public void GetItem()
         {
             ProductCategory category;
             ContainableItem item = null;
@@ -168,6 +181,6 @@ namespace VendingMachine
             }
             }
 
-        }
+        }*/
     }
 }
